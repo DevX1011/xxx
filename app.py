6861aba09e98878e87350081ee4ff1daaa3be323
DevX1011 <t.korkmazz@icloud.com>
2025-07-01 20:29:19 +0200

    app.py

--- app.py
+++ app.py
@@ -68,3 +68,7 @@ if __name__ == '__main__':
         db.create_all()
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
+
+@app.route('/')
+def home():
+    return '🔐 Lizenzserver läuft – API erreichbar!', 200
